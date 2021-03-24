ad23d20a256972344d2d95c8f0e36bfed79fb221
Cotidib <77683770+Cotidib@users.noreply.github.com>
2021-03-24 11:51:33 -0300

    Update validname.cs

--- validname.cs
+++ validname.cs
@@ -1,50 +1,65 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 
-bool IsInitial(string name){
-    return ( name.Length == 2 && Char.IsUpper(name,0) && name.EndsWith(".") );
-}
 
-bool IsWord(string name){
-    return ( name.Length > 2 && Char.IsUpper(name,0) && !name.EndsWith(".") );
-}
-
-bool ValidName(string name) 
+namespace Program
 {
-    string[] splittedName = name.Split(' ');
-    bool isValid = false;
-
-    bool firstElementCondition = IsInitial(splittedName[0]) || IsWord(splittedName[0]) ;
-    bool lastElementCondition = IsWord(splittedName[splittedName.Length - 1]);
-
-    if(splittedName.Length == 3){
-       bool middleElementCondition = IsInitial(splittedName[1]) || ( IsWord(splittedName[0]) && IsWord(splittedName[1]) );
-       if(firstElementCondition && middleElementCondition && lastElementCondition){
-           isValid = true;
-       }
-    }
-
-    else if(splittedName.Length == 2){
-        if(firstElementCondition && lastElementCondition){
-            isValid = true;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ValidName("H. Wells"); // True
+            //ValidName("H. G. Wells"); // True
+            //ValidName("Herbert G. Wells"); // True
+            //ValidName("Herbert George Wells"); // True
+            //ValidName("Herbert"); // False
+            //ValidName("H Wells"); // False
+            //ValidName("H. wells"); // False
+            //ValidName("H. George Wells"); // False
+            //ValidName("H. G. W."); //False
+            //ValidName("Herb. G. Wells"); // False
+            
         }
-    }
-    else{
-        isValid = false;
-    }
 
-    Console.WriteLine(isValid);
-    return isValid;
+        // https://edabit.com/challenge/pcHzxfGheeNE4JDpR
+
+        public static bool IsInitial(string name){
+            return ( name.Length == 2 && Char.IsUpper(name,0) && name.EndsWith(".") );
+        }
+
+        public static bool IsWord(string name){
+            return ( name.Length > 2 && Char.IsUpper(name,0) && !name.EndsWith(".") );
+        }
+
+        public static bool ValidName(string name) 
+        {
+            string[] splittedName = name.Split(' ');
+            bool isValid = false;
+
+            bool firstElementCondition = IsInitial(splittedName[0]) || IsWord(splittedName[0]) ;
+            bool lastElementCondition = IsWord(splittedName[splittedName.Length - 1]);
+
+            if(splittedName.Length == 3){
+            bool middleElementCondition = IsInitial(splittedName[1]) || ( IsWord(splittedName[0]) && IsWord(splittedName[1]) );
+            if(firstElementCondition && middleElementCondition && lastElementCondition){
+                isValid = true;
+            }
+            }
+
+            else if(splittedName.Length == 2){
+                if(firstElementCondition && lastElementCondition){
+                    isValid = true;
+                }
+            }
+            else{
+                isValid = false;
+            }
+
+            Console.WriteLine(isValid);
+            return isValid;
+        }
+
+        
+
+        
+    }
 }
-
-//ValidName("H. Wells"); // True
-//ValidName("H. G. Wells"); // True
-//ValidName("Herbert G. Wells"); // True
-//ValidName("Herbert George Wells"); // True
-//ValidName("Herbert"); // False
-//ValidName("H Wells"); // False
-//ValidName("H. wells"); // False
-//ValidName("H. George Wells"); // False
-//ValidName("H. G. W."); //False
-//ValidName("Herb. G. Wells"); // False
